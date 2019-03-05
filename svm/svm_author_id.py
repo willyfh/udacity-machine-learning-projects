6dc2d467cfac624be37cde1d74056169684851d4
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2019-03-05 18:11:51 +0700

    implement SVM mini project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+
+### Slicing the training dataset down to 1%
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+clf = svm.SVC(kernel='rbf', C=10000.0)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+acc = accuracy_score(pred, labels_test)
+print "accuracy:", acc
+
 #########################################################
 
 

75f1fc570864936b9e2cf63da131d91445898cec
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2019-03-05 23:22:01 +0700

    implement Decision Tree mini project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,14 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc = accuracy_score(labels_test, pred)
+print "accuracy:", acc
 
 #########################################################
 
